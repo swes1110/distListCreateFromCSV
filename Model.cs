e891d56d19677c3b2f74496a83610023f92351c2
Your Name <you@example.com>
2021-02-19 16:48:21 -0500

    Convert to c# app

---
+++ Model.cs
@@ -0,0 +1,11 @@
+using CsvHelper.Configuration.Attributes;
+
+namespace distListCreateFromCSV.Models
+{
+    public class contactModel
+    {
+        public string firstName {get; set;}
+        public string lastName {get; set;}
+        public string emailAddress {get; set;}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Microsoft.Exchange.WebServices.Data;
+using System.Collections.Generic;
+using distListCreateFromCSV.Models;
+
+namespace distListCreateFromCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get username from user
+            System.Console.WriteLine("Please enter the username for a user with administrative permissions to the exchange server: \r\n");
+            string username = Console.ReadLine();
+            //Get password
+            System.Console.WriteLine("\r\nPlease enter the password for the above user");
+            string password = Console.ReadLine();
+
+            //Create the EWS binding
+            ExchangeService service = new ExchangeService();
+            //Set credentials
+            service.Credentials = new WebCredentials(username,password);
+            //Set the service url
+            service.Url = new Uri("https://exchange.theabfm.org/EWS/Exchange.asmx");
+
+            //Read CSV file into variable
+            TextReader reader = new StreamReader("templateFile.csv");
+            CsvHelper.CsvParser parser = new CsvHelper.CsvParser(reader, );
+            var csvReader = new CsvHelper.CsvReader(reader);
+        }
+    }
+}
