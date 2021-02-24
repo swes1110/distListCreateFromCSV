d144feb72ac24bb684dcdef5f6659b04eb004a91
Shawn Reynolds <swes1110@gmail.com>
2021-02-24 15:16:34 -0500

    Move exchange connection to function

--- Program.cs
+++ Program.cs
@@ -12,6 +12,31 @@ namespace distListCreateFromCSV
     {
         static void Main(string[] args)
         {
+            //Call function to read CSV file and store in variable
+            IEnumerable<contact> contacts = readCsv();
+            //Loop thru variable and dispaly data (this is just for testing)
+            foreach (var contact in contacts)
+            {
+                System.Console.Clear();
+                System.Console.WriteLine("First Name: " + contact.firstName);
+                System.Console.WriteLine("Last Name: " + contact.lastName);
+                System.Console.WriteLine("E-Mail Address: " + contact.emailAddress);
+            }
+
+            // connectExchange();
+        }
+
+        static IEnumerable<contact> readCsv()
+        {
+            // Read CSV file
+            TextReader reader = new StreamReader("templateFile.csv");
+            var csvReader = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture);
+            IEnumerable<contact> contacts = csvReader.GetRecords<contact>();
+            return contacts;
+        }
+
+        static void connectExchange()
+        {
             //Get username from user
             System.Console.WriteLine("Please enter the username for a user with administrative permissions to the exchange server: \r\n");
             string username = Console.ReadLine();
@@ -25,11 +50,6 @@ namespace distListCreateFromCSV
             service.Credentials = new WebCredentials(username,password);
             //Set the service url
             service.Url = new Uri("https://exchange.theabfm.org/EWS/Exchange.asmx");
-
-            //Read CSV file into variable
-            TextReader reader = new StreamReader("templateFile.csv");
-            var csvReader = new CsvReader(reader, System.Globalization.CultureInfo.CurrentCulture);
-            var records = csvReader.GetRecords<contact>();
         }
     }
 }
